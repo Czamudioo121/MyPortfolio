4e6e2c4e46b4f2c756a7ec0eedadc3b9d6e872eb
Zamudio <jumbocesar12@gmail.com>
2025-08-03 22:45:31 -0600

    añadidos

---
+++ src/components/generic/AvatarView.jsx
@@ -0,0 +1,28 @@
+import "./AvatarView.scss"
+import React, {useEffect, useState} from 'react'
+import ImageView from "/src/components/generic/ImageView.jsx"
+import * as FaIcons from "react-icons/fa";
+
+
+function AvatarView({ src = "", alt = "", faIcon = "", className = "",  id = null, style = null, imgStyle = null }) {
+    return (
+        <div className={`avatar-view ${className}`}
+             id={id}
+             style={style}>
+            {src && (
+                <ImageView src={src}
+                           alt={alt}
+                          imgStyle = {null}
+                           className={`avatar-view-image-view`}/>
+            )}
+
+            {!src && (
+                <div className={`avatar-icon-view`}>
+                    <i className={`${faIcon}`}/>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default AvatarView
