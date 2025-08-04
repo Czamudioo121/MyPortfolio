8c10ae6a4aae23a941a51eba1b012a669189a382
Zamudio <jumbocesar12@gmail.com>
2025-08-03 22:34:17 -0600

    Primer commit

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+    base: '/portfolio/',
+    plugins: [react()],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        // Split the swiper plugin library into a separate chunk to avoid a large chunk size on index.js
+                        if (id.includes('swiper'))
+                            return 'swiper';
+                        return;
+                    }
+                }
+            }
+        }
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                silenceDeprecations: ["mixed-decls", "color-functions", "global-builtin", "import"],
+            },
+        },
+    },
+})
